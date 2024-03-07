f733ee8051c963bf7903d20b8f4e498f60048cdd
Coralain <carolain.antoc@gmail.com>
2024-03-07 00:07:59 -0500

    PopUp prefab and functions

---
+++ Assets/Scripts/UI/PopUp.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PopUp : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text TMPTitle;
+
+    public delegate void OnOpen();
+    public OnOpen onOpen;
+    public delegate void OnClose();
+    public OnClose onClose;
+    public delegate void OnAccept();
+    public OnAccept onAccept;
+
+    public string Title {
+        get
+        {
+            return TMPTitle.text;
+        }
+        set
+        {
+            TMPTitle.text = value;
+        }
+    }
+    
+
+    public void Open()
+    {
+        gameObject.SetActive(true);
+        onOpen?.Invoke();
+        //play animation
+    }
+
+    public void Close()
+    {
+        onClose?.Invoke();
+        gameObject.SetActive(false);
+        //play animation
+    }
+
+    public void Accept()
+    {
+        onAccept?.Invoke();
+        Close();
+    }
+}

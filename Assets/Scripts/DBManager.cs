7aa163b6fe3a6e22a2147bc5b3282ecfe56829e7
Coralain <carolain.antoc@gmail.com>
2024-03-10 23:57:38 -0500

    CD adding add column script
    not needed now

--- Assets/Scripts/DBManager.cs
+++ Assets/Scripts/DBManager.cs
@@ -56,4 +56,25 @@ public class DBManager : MonoBehaviour
         var playerList = db.Query<Player>($"select * from Player");
         return playerList;
     }
+
+    // // https://stackoverflow.com/questions/18920136/check-if-a-column-exists-in-sqlite
+    // public bool DoFieldExist(string table, string column)
+    // {
+    //     SQLiteConnection connection = GetConnection();
+    //     DataTable ColsTable = connection.GetSchema("Columns");
+
+    //     var data = ColsTable.Select(string.Format("COLUMN_NAME='{1}' AND TABLE_NAME='{0}1'", table, column));
+    //     return data.Length == 1;
+    // }
+
+    // https://stackoverflow.com/questions/1569685/c-sharp-dynamically-add-columns-to-table-in-database
+    public void AddColumn(string table, string column, string columnType)
+    {
+        // if (DoFieldExist(table, column)) return;
+        SQLiteConnection connection = new SQLiteConnection(dbPath);
+
+        SQLiteCommand cmd = new SQLiteCommand(connection);
+        cmd.CommandText = "ALTER TABLE " + table + " ADD COLUMN " + column + " " + columnType + ";";
+        cmd.ExecuteNonQuery();
+    }
 }

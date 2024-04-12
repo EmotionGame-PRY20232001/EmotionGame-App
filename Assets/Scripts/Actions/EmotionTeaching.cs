ea2d81a21bea24f5488dbc73b5df02d6a798395e
Coralain <carolain.antoc@gmail.com>
2024-04-12 01:15:52 -0500

    Emotions Teaching

--- Assets/Scripts/Actions/EmotionTeaching.cs
+++ Assets/Scripts/Actions/EmotionTeaching.cs
@@ -7,20 +7,13 @@ using TMPro;
 
 public class EmotionTeaching : MonoBehaviour
 {
-    public uint NumExercises = 10;
+    public uint NumExcercises = 10;
     protected uint CurrentExercise = 0;
     protected Emotion.EEmotion CurrentEmotion;
+    List<Emotion.EEmotion> EmotionsToLearn;
 
     [SerializeField]
-    protected bool UseRandom = true;
-
-    [SerializeField]
-    protected EmotionPhoto PhotoPrefab;
-    //[SerializeField]
-    //protected List<GameObject> Photos;
-    protected List<Emotion.EEmotion> EmotionsToLearn;
-    [SerializeField]
-    protected GameObject PhotosContent;
+    protected PhotosCollection PhotosContent;
     [SerializeField]
     protected Stepper StepperCont;
 
@@ -31,76 +24,38 @@ public class EmotionTeaching : MonoBehaviour
 
     void Start()
     {
+        EmotionsToLearn = new List<Emotion.EEmotion>();
         SetEmotionExercises();
     }
 
     protected void SetEmotionExercises()
     {
-        Dictionary<Emotion.EEmotion, uint> EmotionExercises;
-        EmotionExercises = new Dictionary<Emotion.EEmotion, uint>();
-        EmotionsToLearn = new List<Emotion.EEmotion>();
-
         var gm = GameManager.Instance;
-        uint numEmotions = (uint)(gm.SelectedEmotions.Count);
-        uint numPerEmotion = NumExercises / numEmotions;
 
-        foreach (Emotion.EEmotion emotion in gm.SelectedEmotions)
+        if (PhotosContent != null)
         {
-            EmotionExercises[emotion] = numPerEmotion;
+            PhotosContent.NumPhotos = NumExcercises;
+            PhotosContent.LoadPhotos();
+
+            EmotionPhoto photo = PhotosContent.Photos.ElementAt(0);
+            if (photo != null)
+                CurrentEmotion = photo.PhotoEmotion;
+
+            foreach(EmotionPhoto _photo in PhotosContent.Photos)
+                LoadExercise(_photo);
         }
-
-        if (NumExercises % numEmotions != 0)
-        {
-            uint aux = NumExercises - (numEmotions * numPerEmotion);
-            var emotions = gm.SelectedEmotions.OrderBy(x => Random.value).ToList();
-
-            for (int i = 0; i < aux; i++)
-            {
-                Emotion.EEmotion emotion = emotions.ElementAt(i);
-                EmotionExercises[emotion] += 1;
-            }
-        }
-
-        CurrentEmotion = gm.SelectedEmotions[0];
-        for (uint i = 0; i < NumExercises; i++)
-            LoadExercise(EmotionExercises);
 
         if (StepperCont != null)
             StepperCont.onStepChange += OnEmotionChanged;
     }
 
-    public void LoadExercise(Dictionary<Emotion.EEmotion, uint> EmotionExercises)
+    public void LoadExercise(EmotionPhoto photo)
     {
-        List<Emotion.EEmotion> emotionsLeft = EmotionExercises.Keys.ToList();
-        if (UseRandom)
-        {
-            emotionsLeft.Remove(CurrentEmotion);
-            CurrentEmotion = emotionsLeft[Random.Range(0, emotionsLeft.Count)];
-        }
-        else
-        {
-            CurrentEmotion = emotionsLeft[0];
-        }
+        if (photo == null) return;
+        if (StepperCont != null)
+            StepperCont.AddStep(photo.gameObject);
 
-
-        if (PhotoPrefab != null && PhotosContent != null)
-        {
-            var photo = Instantiate(PhotoPrefab, PhotosContent.transform);
-            if (photo != null)
-            {
-                photo.SetPhotoEmotion(CurrentEmotion);
-                if (StepperCont != null)
-                    StepperCont.AddStep(photo.gameObject);
-            }
-        }
-
-        EmotionExercises[CurrentEmotion] -= 1;
-        if (EmotionExercises[CurrentEmotion] == 0)
-        {
-            EmotionExercises.Remove(CurrentEmotion);
-        }
-
-        EmotionsToLearn.Add(CurrentEmotion);
+        EmotionsToLearn.Add(photo.PhotoEmotion);
     }
 
     public void OnEmotionChanged()

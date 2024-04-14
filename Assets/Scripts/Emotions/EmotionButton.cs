ec834c7c697500d2830be47748530137bb73a448
Coralain <carolain.antoc@gmail.com>
2024-04-13 21:10:18 -0500

    EmotionButton inherits from EmotionObject

--- Assets/Scripts/Emotions/EmotionButton.cs
+++ Assets/Scripts/Emotions/EmotionButton.cs
@@ -4,49 +4,18 @@ using UnityEngine;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(Button))]
-[RequireComponent(typeof(Image))]
-public class EmotionButton : MonoBehaviour
+public class EmotionButton : EmotionObject
 {
-    public Emotion.EEmotion CurrEmotion;
-    [SerializeField]
-    protected bool IsRandom;
     protected Button Button;
-    protected Image EmotionImage;
     
-    protected void Awake()
+    protected override void Awake()
     {
+        base.Awake();
         Button = gameObject.GetComponent<Button>();
-        EmotionImage = gameObject.GetComponent<Image>();
     }
 
-    private void Start()
+    protected override void Start()
     {
-        //LoadRandom();
-        //LoadImageByEmotion();
-    }
-
-    protected void LoadRandom()
-    {
-        if (IsRandom)
-        {
-            int cant = System.Enum.GetNames(typeof(Emotion.EEmotion)).Length;
-            CurrEmotion = (Emotion.EEmotion)Random.Range(0, cant - 1);
-            Debug.Log(CurrEmotion);
-        }
-    }
-
-    protected void LoadImageByEmotion()
-    {
-        if (GameManager.Instance != null && EmotionImage != null)
-        {
-            EmotionImage.sprite = GameManager.Instance.Emotions[CurrEmotion].Sprite;
-        }
-    }
-
-    public void SetButton(Sprite emotionSprite, Emotion.EEmotion emotion)
-    {
-        EmotionImage.sprite = emotionSprite;
-        CurrEmotion = emotion;
     }
 
     public void PressButton()
---
+++ Assets/Scripts/Emotions/EmotionObject.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+[RequireComponent(typeof(Image))]
+public class EmotionObject : MonoBehaviour
+{
+    [field:SerializeField]
+    public Emotion.EEmotion CurrEmotion { get; protected set; }
+    [SerializeField]
+    protected bool IsRandom;
+    protected Image EmotionImage;
+    protected TMP_Text EmotionName;
+    
+    protected virtual void Awake()
+    {
+        EmotionImage = gameObject.GetComponent<Image>();
+    }
+
+    protected virtual void Start()
+    {
+        if (IsRandom)
+            LoadRandom();
+    }
+
+    protected void LoadRandom()
+    {
+        if (!IsRandom) return;
+
+        int cant = System.Enum.GetNames(typeof(Emotion.EEmotion)).Length;
+        CurrEmotion = (Emotion.EEmotion)Random.Range(0, cant - 1);
+        Debug.Log("[EmotionObject] Loading random: " + CurrEmotion);
+        LoadByEmotion();
+    }
+
+    protected void LoadByEmotion()
+    {
+        if (GameManager.Instance != null && EmotionImage != null)
+        {
+            Emotion.Data emoData = GameManager.Instance.Emotions[CurrEmotion];
+            EmotionImage.sprite = emoData.Sprite;
+            if (EmotionName != null)
+                EmotionName.text = emoData.Name;
+        }
+    }
+
+    public void SetEmotion(Emotion.EEmotion emotion)
+    {
+        CurrEmotion = emotion;
+        LoadByEmotion();
+    }
+
+}

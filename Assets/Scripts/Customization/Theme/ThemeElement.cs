fed9ca0b5e346245e7f973d91fe730559b4f736e
Coralain <carolain.antoc@gmail.com>
2024-04-13 19:38:20 -0500

    theme + gm update
    * theme element: removing rawimage requirecomponent
    * GM: adding default frame

--- Assets/Scripts/Customization/Theme/ThemeElement.cs
+++ Assets/Scripts/Customization/Theme/ThemeElement.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(RawImage))]
 public class ThemeElement : MonoBehaviour
 {
     [SerializeField]

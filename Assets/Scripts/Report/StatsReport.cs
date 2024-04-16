02a1921f77a0e5933cd5341c08daf10458b0ec31
Coralain <carolain.antoc@gmail.com>
2024-04-16 01:32:25 -0500

    StatsReport create

---
+++ Assets/Scripts/Report/StatsReport.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatsReport : Report
+{
+
+
+    protected override void OnFilerDates(List<System.DateTime> dates)
+    {
+    }
+
+    protected override void OnGameToggleChanged(Exercise.EGame game)
+    {
+    }
+
+    //protected void 
+}

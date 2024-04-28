db82adcf84cb521a14e888b176da32286905c9fe
Coralain <carolain.antoc@gmail.com>
2024-04-28 14:54:36 -0500

    Select theme updates UI palette

--- Assets/Scripts/Customization/Theme/ThemeColorFilled.cs
+++ Assets/Scripts/Customization/Theme/ThemeColorFilled.cs
@@ -36,14 +36,25 @@ public class ThemeColorFilled : MonoBehaviour
 
     protected void Start()
     {
-        LoadPlayerTheme();
-    }
-
-    public void LoadPlayerTheme()
-    {
         LoadDefault();
         UpdateColors();
         UpdateFillType();
+    }
+
+    public void LoadPlayerTheme(Theme.EBackground bgId)
+    {
+        LoadByBgId(bgId);
+        UpdateColors();
+        UpdateFillType();
+    }
+
+    protected void LoadByBgId(Theme.EBackground bgId)
+    {
+        var gm = GameManager.Instance;
+        if (gm == null) return;
+
+        CurrentTheme = gm.GetBackgrounds()[bgId].Theme;
+        DefaultTheme = gm.ThemeCustom.Themes[CurrentTheme];
     }
 
     protected void LoadDefault()
@@ -55,9 +66,8 @@ public class ThemeColorFilled : MonoBehaviour
         {
             var player = gm.GetCurrentPlayer();
             Theme.EBackground currentBg = (Theme.EBackground)player.BackgroundId;
-            CurrentTheme = gm.GetBackgrounds()[currentBg].Theme;
+            LoadByBgId(currentBg);
         }
-        DefaultTheme = gm.ThemeCustom.Themes[CurrentTheme];
     }
 
     protected void UpdateColors()
--- Assets/Scripts/Layouts/ThemeLayout.cs
+++ Assets/Scripts/Layouts/ThemeLayout.cs
@@ -20,7 +20,7 @@ public class ThemeLayout : MonoBehaviour
 
     protected void Start()
     {
-        ThemeElements = GetComponentsInChildren<ThemeColorFilled>();
+        ThemeElements = GameObject.FindObjectsOfType<ThemeColorFilled>();
     }
 
     protected void LoadPrefabs()
@@ -31,8 +31,21 @@ public class ThemeLayout : MonoBehaviour
             var instance = Instantiate(themeTogglePrefab, transform);
             instance.GetComponent<Toggle>().group = toggleGroup;
             ToggleTheme themeToggle = instance.GetComponent<ToggleTheme>();
-            themeToggle.Background = Background;
+            themeToggle.Layout = this;
             themeToggle.LoadData(bg.Key, bg.Value);
+        }
+    }
+
+    public void ChangeSelection(Theme.EBackground _theme)
+    {
+        Background.UpdateThemeElement(_theme);
+        if (ThemeElements != null && ThemeElements.Length > 0)
+        {
+            foreach (ThemeColorFilled tcf in ThemeElements)
+            {
+                if (tcf != null)
+                    tcf.LoadPlayerTheme(_theme);
+            }
         }
     }
 
@@ -40,14 +53,5 @@ public class ThemeLayout : MonoBehaviour
     {
         var gm = GameManager.Instance;
         gm.SetCurrentPlayerTheme(Background.Id);
-
-        if (ThemeElements != null && ThemeElements.Length > 0)
-        {
-            foreach(ThemeColorFilled tcf in ThemeElements)
-            {
-                if (tcf != null)
-                    tcf.LoadPlayerTheme();
-            }
-        }
     }
 }

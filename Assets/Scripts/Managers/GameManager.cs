8b60306104f61193792ea8989c25a74dd263c970
Coralain <carolain.antoc@gmail.com>
2024-05-16 02:21:22 -0500

    GM audio sfx dictionary

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -27,6 +27,9 @@ public class GameManager : MonoBehaviour
     [field:SerializeField]
     public CharacterCustomParts CharacterCustom { get; private set; }
 
+    [field:SerializeField]
+    public CustomSfxs SfxsCustom { get; private set; }
+
     private void Awake()
     {
         if (_instance != null && _instance != this)
@@ -121,6 +124,17 @@ public class GameManager : MonoBehaviour
         public Sprite Eyelashes { get; private set; }
         [field:SerializeField][SerializedDictionary("Haircut", "Sprite")]
         public SerializedDictionary<Character.EHairCut, Sprite> HairCuts { get; private set; }
+    }
+
+    [System.Serializable]
+    public struct CustomSfxs
+    {
+        //Button-Toggle
+        [field:SerializeField][SerializedDictionary("Tag", "AudioClip")]
+        public SerializedDictionary<AudioSrcs.ESfxButton, AudioClip> ButtonSfxs { get; private set; }
+        //PopUp
+        [field:SerializeField][SerializedDictionary("Tag", "AudioClip")]
+        public SerializedDictionary<AudioSrcs.ESfxGame, AudioClip> GameSfxs { get; private set; }
     }
 
     public void PauseGame()

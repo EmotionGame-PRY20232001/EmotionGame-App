6d3dc125ee2fd5184edf727c1325cff781f7732f
Coralain <carolain.antoc@gmail.com>
2025-05-13 23:35:55 -0500

    Player button showing btn play
    more intuitive to click to play

--- Assets/Scripts/Buttons/PlayerButton.cs
+++ Assets/Scripts/Buttons/PlayerButton.cs
@@ -17,8 +17,6 @@ public class PlayerButton : MonoBehaviour
     private GameObject optionsContainer;
     [SerializeField]
     protected ThemeButton playerButton;
-    [SerializeField]
-    private Image playIcon;
 
     private Player playerReference;
 
@@ -36,7 +34,6 @@ public class PlayerButton : MonoBehaviour
         nameText.text = player.Name;
         background.sprite = GameManager.Instance.GetBackgrounds()[(Theme.EBackground)player.BackgroundId].Texture;
         guide.SetByJson(player.GuideJSON);
-        playIcon?.CrossFadeAlpha(0.0f, 0.0f, true);
     }
 
     public void ActivateOptions()
@@ -48,7 +45,6 @@ public class PlayerButton : MonoBehaviour
             //SceneManager.LoadScene("MainMenu");
         }
         optionsContainer.SetActive(true);
-        playIcon?.CrossFadeAlpha(1.0f, 0.25f, true);
     }
 
     public void DesactivateOptions()
@@ -56,7 +52,6 @@ public class PlayerButton : MonoBehaviour
         if (optionsContainer != null && optionsContainer.activeSelf)
         {
             optionsContainer.SetActive(false);
-            playIcon?.CrossFadeAlpha(0.0f, 0.25f, true);
         }
     }
 
